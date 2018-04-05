905dad6e73d15810c036592430b4b5f9d936ae96
Azmadeu <doubleworkz@mail.ru>
2018-04-05 08:05:42 +0300

    create basic structure with HTML5&CSS3

---
+++ index.js
@@ -0,0 +1,40 @@
+var ab = [];
+var equal = 0;
+
+function sum(a, b) {
+    equal = a + b;
+}
+
+function quotient(a, b) {
+    equal = a / b;
+}
+
+function difference() {
+
+}
+
+window.onload = function () {
+    var button = $('button');
+    var ac = $('button')[0];
+    var ce = $('button')[1];
+    var divide = $('button')[2];
+    var x = $('button')[3];
+    var seven = $('button')[4];
+    var eight = $('button')[5];
+    var nine = $('button')[6];
+    var minus = $('button')[7];
+    var four = $('button')[8];
+    var five = $('button')[9];
+    var six = $('button')[10];
+    var plus = $('button')[11];
+    var one = $('button')[12];
+    var two = $('button')[13];
+    var three = $('button')[14];
+    var zero = $('button')[15];
+    var tochka = $('button')[16];
+    var equal = $('button')[17];
+
+    buttons.onclick = function () {
+
+    }
+};
